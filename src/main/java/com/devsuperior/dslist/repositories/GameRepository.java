77a77ac0aad52ef1ccadb0ba9e5a596146705e7e
Gabriel Resende <gabriellrr2005@gmail.com>
2023-07-12 17:09:49 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dslist.entities.Game;
 
 public interface GameRepository  extends JpaRepository<Game, Long>{	
+	
 }
